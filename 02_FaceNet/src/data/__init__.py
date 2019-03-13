797305537c7d2110eb7e0c6543ba65d1f7bf37a6
haoxin <haoxin1@kingsoft.com>
2019-03-13 15:09:35 +0800

    verify result on checkpoint 1

--- 02_FaceNet/src/data/__init__.py
+++ 02_FaceNet/src/data/__init__.py
@@ -4,31 +4,31 @@ import numpy as np
 from numpy import genfromtxt
 
 WEIGHTS = [
-        'conv1', 'bn1', 'conv2', 'bn2', 'conv3', 'bn3',
-        'inception_3a_1x1_conv', 'inception_3a_1x1_bn',
-        'inception_3a_pool_conv', 'inception_3a_pool_bn',
-        'inception_3a_5x5_conv1', 'inception_3a_5x5_conv2', 'inception_3a_5x5_bn1', 'inception_3a_5x5_bn2',
-        'inception_3a_3x3_conv1', 'inception_3a_3x3_conv2', 'inception_3a_3x3_bn1', 'inception_3a_3x3_bn2',
-        'inception_3b_3x3_conv1', 'inception_3b_3x3_conv2', 'inception_3b_3x3_bn1', 'inception_3b_3x3_bn2',
-        'inception_3b_5x5_conv1', 'inception_3b_5x5_conv2', 'inception_3b_5x5_bn1', 'inception_3b_5x5_bn2',
-        'inception_3b_pool_conv', 'inception_3b_pool_bn',
-        'inception_3b_1x1_conv', 'inception_3b_1x1_bn',
-        'inception_3c_3x3_conv1', 'inception_3c_3x3_conv2', 'inception_3c_3x3_bn1', 'inception_3c_3x3_bn2',
-        'inception_3c_5x5_conv1', 'inception_3c_5x5_conv2', 'inception_3c_5x5_bn1', 'inception_3c_5x5_bn2',
-        'inception_4a_3x3_conv1', 'inception_4a_3x3_conv2', 'inception_4a_3x3_bn1', 'inception_4a_3x3_bn2',
-        'inception_4a_5x5_conv1', 'inception_4a_5x5_conv2', 'inception_4a_5x5_bn1', 'inception_4a_5x5_bn2',
-        'inception_4a_pool_conv', 'inception_4a_pool_bn',
-        'inception_4a_1x1_conv', 'inception_4a_1x1_bn',
-        'inception_4e_3x3_conv1', 'inception_4e_3x3_conv2', 'inception_4e_3x3_bn1', 'inception_4e_3x3_bn2',
-        'inception_4e_5x5_conv1', 'inception_4e_5x5_conv2', 'inception_4e_5x5_bn1', 'inception_4e_5x5_bn2',
-        'inception_5a_3x3_conv1', 'inception_5a_3x3_conv2', 'inception_5a_3x3_bn1', 'inception_5a_3x3_bn2',
-        'inception_5a_pool_conv', 'inception_5a_pool_bn',
-        'inception_5a_1x1_conv', 'inception_5a_1x1_bn',
-        'inception_5b_3x3_conv1', 'inception_5b_3x3_conv2', 'inception_5b_3x3_bn1', 'inception_5b_3x3_bn2',
-        'inception_5b_pool_conv', 'inception_5b_pool_bn',
-        'inception_5b_1x1_conv', 'inception_5b_1x1_bn',
-        'dense_layer'
-    ]
+        'conv1', 'bn1', 'conv2', 'bn2', 'conv3', 'bn3']
+    #     'inception_3a_1x1_conv', 'inception_3a_1x1_bn',
+    #     'inception_3a_pool_conv', 'inception_3a_pool_bn',
+    #     'inception_3a_5x5_conv1', 'inception_3a_5x5_conv2', 'inception_3a_5x5_bn1', 'inception_3a_5x5_bn2',
+    #     'inception_3a_3x3_conv1', 'inception_3a_3x3_conv2', 'inception_3a_3x3_bn1', 'inception_3a_3x3_bn2',
+    #     'inception_3b_3x3_conv1', 'inception_3b_3x3_conv2', 'inception_3b_3x3_bn1', 'inception_3b_3x3_bn2',
+    #     'inception_3b_5x5_conv1', 'inception_3b_5x5_conv2', 'inception_3b_5x5_bn1', 'inception_3b_5x5_bn2',
+    #     'inception_3b_pool_conv', 'inception_3b_pool_bn',
+    #     'inception_3b_1x1_conv', 'inception_3b_1x1_bn',
+    #     'inception_3c_3x3_conv1', 'inception_3c_3x3_conv2', 'inception_3c_3x3_bn1', 'inception_3c_3x3_bn2',
+    #     'inception_3c_5x5_conv1', 'inception_3c_5x5_conv2', 'inception_3c_5x5_bn1', 'inception_3c_5x5_bn2',
+    #     'inception_4a_3x3_conv1', 'inception_4a_3x3_conv2', 'inception_4a_3x3_bn1', 'inception_4a_3x3_bn2',
+    #     'inception_4a_5x5_conv1', 'inception_4a_5x5_conv2', 'inception_4a_5x5_bn1', 'inception_4a_5x5_bn2',
+    #     'inception_4a_pool_conv', 'inception_4a_pool_bn',
+    #     'inception_4a_1x1_conv', 'inception_4a_1x1_bn',
+    #     'inception_4e_3x3_conv1', 'inception_4e_3x3_conv2', 'inception_4e_3x3_bn1', 'inception_4e_3x3_bn2',
+    #     'inception_4e_5x5_conv1', 'inception_4e_5x5_conv2', 'inception_4e_5x5_bn1', 'inception_4e_5x5_bn2',
+    #     'inception_5a_3x3_conv1', 'inception_5a_3x3_conv2', 'inception_5a_3x3_bn1', 'inception_5a_3x3_bn2',
+    #     'inception_5a_pool_conv', 'inception_5a_pool_bn',
+    #     'inception_5a_1x1_conv', 'inception_5a_1x1_bn',
+    #     'inception_5b_3x3_conv1', 'inception_5b_3x3_conv2', 'inception_5b_3x3_bn1', 'inception_5b_3x3_bn2',
+    #     'inception_5b_pool_conv', 'inception_5b_pool_bn',
+    #     'inception_5b_1x1_conv', 'inception_5b_1x1_bn',
+    #     'dense_layer'
+    # ]
 
 WEIGHTS_TEST = [
         'conv1', 'inception_3a_1x1_bn', 'inception_3a_1x1_conv', 'dense_layer'
